2c14ef78fdbc093b0e6505936388878343727a43
Ali <74864133+Ali-Eksioglu@users.noreply.github.com>
2022-03-16 23:04:56 +0300

    kapılar dğeişti

--- Assets/Scripts/fırca.cs
+++ Assets/Scripts/fırca.cs
@@ -5,27 +5,13 @@ using DG.Tweening;
 
 public class fırca : MonoBehaviour
 {
-    [SerializeField] Vector3 first;
-    [SerializeField] Vector3 last;
-    bool allow = true;
+    [SerializeField] Transform Firca;
 
+    [SerializeField] Vector3 rotateDegeri;
 
-    void Update()
+    private void Start()
     {
-        if (allow)
-        {
-            transform.DOLocalRotate(first, 1).OnComplete(Back);
-            allow = false;
-        }
+        Firca.DOLocalRotate(rotateDegeri, 1f).SetLoops(-1, LoopType.Yoyo);
     }
 
-    void Back()
-    {
-        transform.DOLocalRotate(last,1).OnComplete(StartAgain);
-    }
-
-    void StartAgain()
-    {
-        allow = true;
-    }
 }
