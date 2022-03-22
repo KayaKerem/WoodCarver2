26bea7cd22ce3cac1e970f9fd584e5cc73526c52
Patatesk <mfurkanzengin68@gmail.com>
2022-03-22 23:48:47 +0300

    sadasda

--- Assets/Scripts/Testere.cs
+++ Assets/Scripts/Testere.cs
@@ -7,12 +7,16 @@ public class Testere : MonoBehaviour
 {
     [SerializeField] Transform _modelTestere;
     [SerializeField] Transform move;
-
+    [SerializeField] Vector3 move2;
 
     private void Start()
     {
         _modelTestere.DOLocalMove(move.localPosition, 0.5f).SetLoops(-1, LoopType.Yoyo);
     }
 
-    
+    private void Update()
+    {
+        transform.Rotate(move2,Space.Self);
+    }
+
 }
